1c3d4bb0b3e311411ff1bdbe72b178dc94072560
Jack Beeken <beekenj@gmail.com>
2023-04-02 10:49:55 -0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Blank page. Oh the tyranny!
+    </div>
+  );
+}
+
+export default App;
